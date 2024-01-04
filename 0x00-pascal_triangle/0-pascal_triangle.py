ed39bb6394be232fd5fc97a9d9effcd5ce2fba15
Smart  Raysam <engsamtob@yahoo.com>
2024-01-04 16:15:12 +0100

    format coding style

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 """A script that computes the Pascal's triangle of a given number"""
+
 def pascal_triangle(n):
     """A function that returns a list of lists of integers
     representing the Pascal’s triangle of n"""
     if n <= 0:
         return []
+    
     pascal = [[1]]
     for i in range(1, n):
         row = [1]
         prev = pascal[i - 1]
+        
         for j in range(len(prev)):
             new = prev[j] + prev[j + 1] if j != len(prev) - 1 else 1
             row.append(new)
