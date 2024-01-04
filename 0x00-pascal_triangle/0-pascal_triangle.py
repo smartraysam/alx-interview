39fe8b2f27107ece97f8c220da0f74a87dba6e5c
Smart  Raysam <engsamtob@yahoo.com>
2024-01-04 17:00:59 +0100

    resolve code style

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,31 +3,16 @@
 
 
 def pascal_triangle(n):
-    """
-    Function that returns a list of lists of integers
-    representing the Pascal’s triangle of n
-    """
-    # initialize list to store Pascal's triangle
-    triangle = []
-
-    # check if n is less than or equal to zero and return empty list
+    """A function that returns a list of lists of integers
+    representing the Pascal’s triangle of n"""
     if n <= 0:
-        return triangle
-
-    # loop through n and initialize list to store numbers
-    for i in range(n):
-        row = []
-
-        # loop through previous loop and append 1 if first or last element
-        for j in range(i + 1):
-            if j == 0 or j == i:
-                row.append(1)
-
-            # if not first or last take sum of both
-            else:
-                row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-
-        # add the sum to the triangle
-        triangle.append(row)
-
-    return triangle
+        return []
+    pascal = [[1]]
+    for i in range(1, n):
+        row = [1]
+        prev = pascal[i - 1]
+        for j in range(len(prev)):
+            new = prev[j] + prev[j + 1] if j != len(prev) - 1 else 1
+            row.append(new)
+        pascal.append(row)
+    return pascal
